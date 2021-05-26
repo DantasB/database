eba5bcfd24c3b7d5f46df4cb83aee59013b097ac
carlos henrique <carloshenriquefbf@poli.ufrj.br>
2021-05-26 01:13:14 -0300

    Change sql queries

--- Trabalho_Final/questao_15.sql
+++ Trabalho_Final/questao_15.sql
@@ -1,3 +1,6 @@
+SELECT *
+FROM Duplicata
+
 INSERT INTO Duplicata (Dt_Vencimento, Vl_Valor, Cd_NF, Dt_Pagamento, Vl_Pagamento)
 VALUES (CAST(DATEADD(day, 15, GETDATE()) AS DATETIME), 40, 1, NULL, NULL),
 	   	 (CAST(DATEADD(day, -15, GETDATE()) AS DATETIME), 50, 2, NULL, NULL)
