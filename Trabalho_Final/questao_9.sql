92fb196f87fd0377047c57a6c484b86ed9c0abd5
Carlos Henrique <64883625+carloshenriquefbf@users.noreply.github.com>
2021-05-28 16:55:30 -0300

    Update questao_9.sql
    Update transaction query

--- Trabalho_Final/questao_9.sql
+++ Trabalho_Final/questao_9.sql
@@ -1,15 +1,25 @@
-BEGIN TRANSACTION 
-UPDATE NotaFiscal 
-SET Tp_EntradaSaida=1
-WHERE CD_NF = 4040;
-COMMIT TRANSACTION;
-
-SELECT * FROM NotaFiscal 
-
-BEGIN TRANSACTION 
-UPDATE NotaFiscal 
-SET Tp_EntradaSaida=0
-WHERE CD_NF = 4040;
-ROLLBACK;
-
-SELECT * FROM NotaFiscal 
+BEGIN TRANSACTION insercaoVeiculo
+    INSERT INTO [dbo].[Veiculo] (
+		[Cd_Placa], 
+		[Cd_Transportadora], 
+		[Nm_Especie],
+		[Nm_Marca],
+		[Cd_UF]
+		) 
+		VALUES (
+		N'LAB2048',
+		1, 
+		N'Conjugado',
+		N'Volkswagen',
+		1
+		)
+    IF(SELECT COUNT(Nm_Especie) from Veiculo where Nm_Especie=N'Conjugado') > 1
+        BEGIN
+        ROLLBACK TRANSACTION
+        PRINT 'Apenas um veiculo conjugado permitido.'
+        END
+    ELSE
+        BEGIN
+        PRINT 'Inclusao bem sucedida'
+        COMMIT TRANSACTION insercaoVeiculo
+        END
